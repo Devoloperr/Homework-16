8ad4d984f30d4ee5496bcf94cce99f389f6de8ab
Devoloperr <mirkamilezizli085@gmail.com>
2024-06-14 16:26:27 +0400

    Homework 16

---
+++ 1app.js
@@ -0,0 +1,2 @@
+let names = "Mirkamil Azizli"
+console.log(names.replace( "Mirkamil Azizli","Azizli Mirkamil") );
---
+++ app.js
@@ -0,0 +1,5 @@
+let text = "Mirkamil Azizi";
+const word = text.split(" ");
+let name = word[0][0];
+let surname = word[1][0];
+console.log(name,surname);
